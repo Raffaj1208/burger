3661967a54e44a1283fd0e8ca5b8403cde53f671
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-08-03 14:11:12 -0700

    saved app with backup file

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+var mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "@Rebirth03",
+    database: "burgers_db"
+  });
+}
+connection.connect();
+module.exports = connection;
---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var routes = require('./controllers/burgers_controller.js');
+var port = process.env.port || 3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+app.use(routes);
+
+app.listen(port, function(){
+    console.log('server listening on: http://localhost: ' + port);
+});
